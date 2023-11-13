31da9f8934a74423c6a7c3847c731f3948723cfa
Vin <126232040+Chuwie2k@users.noreply.github.com>
2023-11-14 03:19:43 +0800

    Need some CSS changes but validation and design is good now

--- JUSONITELEC1CLAB2/Models/Instructor.cs
+++ JUSONITELEC1CLAB2/Models/Instructor.cs
@@ -17,19 +17,19 @@ namespace JUSONITELEC1CLAB2.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "First Name is required")]
+        [Required(ErrorMessage = "First Name is required!!!")]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Last Name is required")]
+        [Required(ErrorMessage = "Last Name is required!!!")]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
         [Display(Name = "Is Tenured")]
         public IsTenured IsTenured { get; set; }
-        [Required(ErrorMessage = "Rank is required")]
+        [Required(ErrorMessage = "Rank is required!!!")]
         public Rank Rank { get; set; }
 
-        [Required(ErrorMessage = "Hiring Date is required")]
+        [Required(ErrorMessage = "Hiring Date is required!!!")]
         public DateTime HiringDate { get; set; }
 
 
